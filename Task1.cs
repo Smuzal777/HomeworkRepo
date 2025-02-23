a0f6a11cd38f9e548a5b1a09cbe584b0d90f1ead
Софья Музалева <smuzal@yandex.ru>
2025-02-23 13:55:54 +0300

    Initial commit

---
+++ Task1.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson1
+{
+    internal class Task1
+    {
+        /*Написать программу, которая выводит на экран фразу "Hello, world!". После этого с клавиатуры должно быть считано имя пользователя (не забывайте про понятный ввод!) и выведена строка "Hello, имя_пользователя!".
+         *
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя:");
+            string s = Console.ReadLine();
+            Console.WriteLine($"Hello, {s}!");
+        }
+    }
+}
+/*
+ * Введите имя:
+Маша
+Hello, Маша!
+
+Введите имя:
+Наташа
+Hello, Наташа!
+
+Введите имя:
+Петя
+Hello, Петя!
+*/
